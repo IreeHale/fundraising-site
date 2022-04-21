1856bc23b0f8d9f6c92fac53bdab307763febe0f
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2022-04-21 00:14:34 -0400

    working on modal functionality

---
+++ index.js
@@ -0,0 +1,10 @@
+let modal = document.querySelector(".modal-container");
+let close = document.querySelector(".close");
+let open = document.querySelector(".reward");
+
+
+
+
+close.addEventListener("click", () => {
+    modal.style.display = "none";
+});
