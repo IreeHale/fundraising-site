b8b7c1cc13df2a518adc44a0b6ed8cc2b46c8a18
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2022-04-21 20:06:40 -0400

    working on buttons to show modal

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let modal = document.querySelector(".modal-container");
-let close = document.querySelector(".close");
-let open = document.querySelector(".reward");
+const modal = document.querySelector(".modal-container");
+const close = document.querySelector(".close");
+const reward = document.getElementsByClassName(".reward")[1];
+const reward2 = document.getElementsByClassName(".reward")[2];
 
 
 
