21904f7040d9cc68fe1c586a119bb33810d09bc3
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2022-04-28 19:16:00 -0400

    wokring on modal input and buttons

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-let close = document.querySelector('.close');
-let modalOpen = document.querySelector('.modal-container');
 let reward = document.getElementById('reward1');
 let reward2 = document.getElementById('reward2');
 let reward3 = document.getElementById('reward3');
-let thankyou = document.getElementById('thankyou-container');
-let thankyouBtn = document.getElementById('ty-btn');
+let close = document.querySelector('.close');
+let modalOpen = document.querySelector('.modal-container');
 let radio1 = document.getElementById('radio1');
 let pledge1 = document.getElementById('pledge1');
 let radio2 = document.getElementById('radio2');
 let pledge2 = document.getElementById('pledge2');
-let donation1 = document.getElementById('donation2');
+let donation1 = document.getElementById('donation-amount2');
+let radio3 = document.getElementById('radio3');
+let pledge3 = document.getElementById('pledge3');
+let donation2 = document.getElementById('donation-amount3');
+let thankyou = document.getElementById('thankyou-container');
+let thankyouBtn = document.getElementById('ty-btn');
+
 
 
 /*Open modal with a click and close with a click*/
@@ -30,40 +34,42 @@ reward3.addEventListener('click', () => {
 });
 
 
-
 /*Pledge with no reward*/
 
 radio1.checked = false;
 
 radio1.addEventListener('click', () =>{
     pledge1.style.borderColor = "hsl(176, 50%, 47%)";
-});
-
-radio1.addEventListener('click', () => {
     thankyou.style.display = 'block';
 });
 
 /*$25 Pledge with reward*/
 radio2.checked = false;
+donation1.style.display = 'none';
 
 radio2.addEventListener('click', () =>{
+    donation1.style.display='block';
     pledge2.style.borderColor = "hsl(176, 50%, 47%)";
 });
 
-radio2.addEventListener('click', () =>{
-    thankyou.style.display = 'block';
-});
-
 /*$75 Pledge with reward*/
+radio3.checked = false;
 
+radio3.addEventListener('click', () =>{
+    thankyou.style.display = 'block';
+    pledge3.style.display = 'block';
+    pledge3.style.borderColor = "hsl(176, 50%, 47%)";
+});
 
 
 /*Thank you modal functionality*/
 
-thankyouBtn.addEventListener('click', () => {
+thankyou.addEventListener('click', () => {
     thankyou.style.display = 'none';
+    radio1.checked = false;
+    pledge1.style.borderColor = 'hsl(0,0%,83%)';
 });
 
-thankyou.addEventListener('click', () => {
+thankyouBtn.addEventListener('click', () => {
     thankyou.style.display = 'none';
 });
