aaf2f4c45e644674b0387e2ec7f3bfb58265c0a3
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2022-04-29 04:01:03 -0400

    working on the book button and the back button

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let book = document.getElementById('book');
+let back = document.getElementById('back');
 let reward = document.getElementById('reward1');
 let reward2 = document.getElementById('reward2');
 let reward3 = document.getElementById('reward3');
@@ -23,6 +25,8 @@ let thankyouBtn = document.getElementById('ty-btn');
 close.addEventListener('click', () => {
     modalOpen.style.display = 'none';
 });
+
+modalOpen.style.display = 'none';
 
 reward.addEventListener('click', () => {
     modalOpen.style.display = 'block';
